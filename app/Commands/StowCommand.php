5e44600b021b5322eb13f672a0f229413527b39a
RVxLab <code@rvx.works>
2025-10-05 17:11:13 +1000

    feat: start implementing stow command

--- app/Commands/StowCommand.php
+++ app/Commands/StowCommand.php
@@ -4,13 +4,35 @@ declare(strict_types=1);
 
 namespace App\Commands;
 
+use DirectoryIterator;
 use Illuminate\Console\Command;
 
 final class StowCommand extends Command
 {
-    protected $signature = 'stow';
+    protected $signature = <<<SIG
+        stow
+        {--d|dir= : The directory that contains your dotfiles. Defaults to the current directory}
+        {--t|target= : The directory to stow into. Defaults to the parent of the current directory}
+
+    SIG;
 
     protected $description = 'Stow the dotfiles';
 
-    public function handle(): void {}
+    public function handle(): void
+    {
+        $pwd = getcwd();
+
+        $stowDir = (string) ($this->option('dir') ?? $pwd);
+        $this->option('target') ?? "$pwd/..";
+
+        $dirIter = new DirectoryIterator($stowDir);
+
+        foreach ($dirIter as $dir) {
+            if ($dir->isDot()) {
+                continue;
+            }
+
+            dump($dir->getPathname());
+        }
+    }
 }
